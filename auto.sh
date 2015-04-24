8263961ec760787be63caaf776bf517576222dba
Giancarlo <sakya_stelios@hotmail.com>
2015-04-24 13:05:19 -0500

    Commit 24_04_2015

---
+++ auto.sh
@@ -0,0 +1,12 @@
+now="$(date +'%d_%m_%Y')"
+fili=$now
+
+touch $fili.html
+
+echo "We are excited to launch our new company and product Ooooh" >> $fili.html
+echo  "After being featured in too many magazines to mention and having created an online stir," >> $fili.html
+echo  "we know that Ooooh is going to be big. You may have seen us in the Dinosaurs’ Den where " >> $fili.html
+
+git add --all
+git commit -m "Commit $now"
+git push origin master
