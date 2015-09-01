4da22c64dddd622af1bdb660d6239d1d1f39f8fc
Giancarlo <sakya_stelios@hotmail.com>
2015-09-01 13:40:07 -0500

    Commit 01_09_2015_13_40_07

--- auto.sh
+++ auto.sh
@@ -8,7 +8,7 @@ echo "We are excited to launch our new company and product Ooooh" >> $fili.html
 echo  "After being featured in too many magazines to mention and having created an online stir," >> $fili.html
 echo  "we know that Ooooh is going to be big. You may have seen us in the Dinosaurs’ Den where " >> $fili.html
 
-curl http://www.microsoft.com >> $fili.html
+curl http://www.microsoft.com/en/us/default.aspx?redir=true >> $fili.html
 
 git add --all
 git commit -m "Commit $fordate"
